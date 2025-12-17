fa24e54ad6a5f2f84cecbd6dde93022538367410
James Gallegos <james.gallegos1618@gmail.com>
2025-12-17 14:23:08 -0800

    Create sensor.h

---
+++ Inc/sensor.h
@@ -0,0 +1,14 @@
+/*
+ * sensor.h
+ *
+ *  Created on: Dec 16, 2025
+ *      Author: jamesgallegos
+ */
+
+#ifndef INC_SENSOR_H_
+#define INC_SENSOR_H_
+
+void sensor_init(void);
+
+
+#endif /* INC_SENSOR_H_ */
